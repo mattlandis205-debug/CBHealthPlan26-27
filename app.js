afd3b778e51cb6874fe7363861b9b8551d97e06e
Landis Family <mattlandis205@gmail.com>
2026-08-08 10:53:47 -0400

    Change Acupuncture Explorer display to generic Covered note without spec price

--- app.js
+++ app.js
@@ -758,7 +758,7 @@ function renderExplorer() {
       if (chiroVal === 0) inText = '100% Covered<br><span style="font-size: 0.7rem; opacity: 0.8;">(See SPD for details)</span>';
       else inText = `$${chiroVal} Copay<br><span style="font-size: 0.7rem; opacity: 0.8;">(See SPD for details)</span>`;
     } else if (activeBenefit === 'acupuncture') {
-      inText = `$${inVal} Copay<br><span style="font-size: 0.7rem; opacity: 0.8;">(Specialist Copay)</span>`;
+      inText = 'Covered<br><span style="font-size: 0.675rem; line-height: 1.35; display: inline-block; opacity: 0.85; font-weight: 500;">Covered in-network; limitations may apply. Contact Luminare directly to inquire.</span>';
     } else if (inVal === 0) {
       inText = '100% Covered ($0)';
     } else if (inVal !== null) {
